b3bc9cfe98d1b172fd343e34ac8fa481474347e8
Zahnen Garner <zahnen@gmail.com>
2021-11-26 15:47:21 -0800

    generate project list dynamically with Prismic

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,30 +1,33 @@
-import Link from "next/link";
-import Layout from "../../components/layout";
-import ProjectCard from "../../components/projectCard";
+import Layout from "../../components/Layout/layout";
+import ProjectCard from "../../components/ProjectCard/projectCard";
+import { Client } from "../../utils/prismicHelpers";
+import Prismic from "@prismicio/client";
+import styles from "./projectpage.module.css";
 
-export default function Projects() {
+export default function Projects({ projects }) {
   return (
     <Layout>
-      <ProjectCard
-        projectInfo="Curo is a project management tool for crafters on the cusp of entrepreneurship. Curo offers kanban-style task management, cost-tracking, and financial analysis tools to help users refine their process and stay organized."
-        projectImg="/images/CuroLogo.svg"
-        projectUrl="/projects/curo"
-      />
-      <ProjectCard
-        projectInfo="Guestly is a customer relationship management tool designed for the hospitality industry. Guestly allows hotel staff to manage records of guest stays, track revenue performance, and sort through historical guest and reservation data."
-        projectImg="/images/GuestlyLogo.svg"
-        projectUrl="/projects/guestly"
-      />
-      <ProjectCard
-        projectInfo="Sous Chef simplifies the art of making food. Sous Chef is a straightforward recipe manager/HTML scraper that helps declutter online recipes & simplify the process of keeping them."
-        projectImg="/images/SousChefLogo.svg"
-        projectUrl="/projects/souschef"
-      />
-      <ProjectCard
-        projectInfo="Paro exists to bridge the preferential rift between analog and digital listening. Paro allows users to discover new music digitally to connect with physically"
-        projectImg="/images/ParoLogo.svg"
-        projectUrl="/projects/paro"
-      />
+      <div className={styles.projectGrid}>
+        {projects.map((project) => (
+          <ProjectCard src={project.data.logo.url} slug={project.slugs[0]} />
+        ))}
+      </div>
     </Layout>
   );
+}
+
+export async function getStaticProps(context) {
+  const document = await Client().query(
+    Prismic.Predicates.at("document.type", "project")
+  );
+
+  if (!document) {
+    return {
+      notFound: true,
+    };
+  } else {
+    return {
+      props: { projects: document.results },
+    };
+  }
 }
